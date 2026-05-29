55aa2536296cdc8a3b2c06a038009878794be1fe
Tobias Bungard <tobi-bungard@t-online.de>
2026-05-29 10:40:37 +0000

    feat: nix packages

--- flake.nix
+++ flake.nix
@@ -15,13 +15,55 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = mkPkgs system;
+        zigDeps = pkgs.callPackage ./deps.nix {};
       in {
         devShells.default = pkgs.mkShell {
           name = "impldb";
 
           buildInputs = with pkgs; [
+            lldb
             zig
+            zon2nix
           ];
+        };
+
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            name = "dbimpl";
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.zig ];
+
+            buildPhase = ''
+              zig build -Doptimize=ReleaseSafe --system ${zigDeps}
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp zig-out/bin/dbimpl $out/bin/
+            '';
+          };
+
+          benchmark = pkgs.stdenv.mkDerivation {
+            name = "dbimpl-benchmark";
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.zig ];
+
+            buildPhase = ''
+              zig build -Doptimize=ReleaseFast --system ${zigDeps}
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp zig-out/bin/benchmark $out/bin/
+            '';
+          };
+        };
+
+        apps = {
+          benchmark = {
+            type = "app";
+            program = "${self.packages.${system}.benchmark}/bin/benchmark";
+          };
         };
       }
   );
